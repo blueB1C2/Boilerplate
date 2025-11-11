53ffc2b4b1b55c593908d24eb3a657c605333348
blueB1C2 <tardiscar@gmail.com>
2025-11-11 14:24:03 -0500

    Make structure blank
    contained stuff from the avatar I was using to co-develop this script

--- Boilerplate/bpactionstructure.lua
+++ Boilerplate/bpactionstructure.lua
@@ -3,60 +3,6 @@ local blank = action_wheel:newAction()
 	:hoverColor(0,0,0)
 	:color(0,0,0)
 
-local homeButton = action_wheel:newAction()
-	:title("Home")
-	:item("red_bed")
-	:hoverColor(1,1,1)
-	:onLeftClick(function() action_wheel:setPage("Main") end)
-
-local inkAction = action_wheel:newAction()
-	:title(toJson({
-		{text = "INK!\n\n", bold = true, italic = true, color = "red"},
-		{text = "Cannot ink outside of water.\n\n", bold = false, color = "white"},
-		{text = "Alternatively, right click while\nholding an ink sac.", bold = false, color = "gray"}
-	}))
-	:item("ink_sac")
-	:hoverColor(vectors.hexToRGB("ff5e00"))
-	:onLeftClick(function() pings.inkItUp() end)
-
-local bloopaAction = action_wheel:newAction()
-	:title(toJson({
-		{text = "Bloopa!", italic = true, color = "aqua"},
-	}))
-	:item("water_bucket")
-	:hoverColor(0,1,1)
-	:onLeftClick(function() pings.bloopa() end)
-
-local changeSquidAction = action_wheel:newAction()
-	:title(toJson({
-		{text = "Bloopa Appearance", italic = true, color = "aqua"},
-	}))
-	:item("squid_spawn_egg")
-	:toggleItem("glow_squid_spawn_egg")
-	:color(vectors.hexToRGB("21497B"))
-	:toggleColor(vectors.hexToRGB("119F36"))
-	:onToggle(pings.setBloopaTexture)
-	:toggled(config:load("GlowBloopa"))
-
-local changeWhirlpoolAction = action_wheel:newAction()
-	:title(toJson({
-		{text = "Toggle Bubbles", italic = true, color = "blue"},
-	}))
-	:item("soul_sand")
-	:toggleItem("magma_block")
-	:color(vectors.hexToRGB("573724"))
-	:toggleColor(vectors.hexToRGB("15a5d1"))
-	:onToggle(pings.setWhirlpoolBubbles)
-	:toggled(config:load("WhirlpoolBubbles"))
-
-local singAction = action_wheel:newAction()
-	:title(toJson({
-			{text = "Play Singing animation", bold = true, color = "blue"}
-	}))
-	:item("music_disc_blocks")
-	:toggleItem("music_disc_cat")
-	:onToggle(pings.sing)
-
 --[[	Menu structure
 		Uses a heirarchy system similar to JSON to automatically construct a menu system.
 
@@ -93,34 +39,9 @@ node = {
 ]]--
 
 local mainMenu = {
-	name = "Homepage",
+	name = "Main",
 	params = {}, -- leave this blank for the home page
 	contains = {
-		inkAction,
-		bloopaAction,
-		{
-			name = "Settings Page",
-			params = {
-				title = toJson({
-					{text = "Settings", italic = true, color = "blue"},
-				}),
-				item = "comparator", 
-				hoverColor = vectors.hexToRGB("5555FF")
-			},
-			contains = { 
-				blank, changeWhirlpoolAction, changeSquidAction
-			}
-		}, 
-		{
-			name = "Animations Page",
-			params = {
-				title = toJson({
-					{text = "Animations", italic = true, color = "dark_green"},
-				}), 
-				item = "armor_stand", 
-				hoverColor = vectors.hexToRGB("47A036")
-			},
-			contains = {blank, singAction}
 		},
 	}
 }

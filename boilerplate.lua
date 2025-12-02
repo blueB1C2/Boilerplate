de981acdaa17ef3bf3a9257fb5cd679629c1a918
blueB1C2 <tardiscar@gmail.com>
2025-12-02 15:04:01 -0500

    Add comments

--- boilerplate.lua
+++ boilerplate.lua
@@ -32,19 +32,19 @@ local libraries = {
 local doTick = {}
 local doRender = {}
 
-for _, library in pairs(libraries) do
+for _, library in pairs(libraries) do -- requires everything in the library list, skips if it doesn't exist
 	local pathLibrary = path .. library
 	if pcall(require, pathLibrary) then 
 		local reqMod = require(pathLibrary)
 		bp[library] = reqMod
 		if type(reqMod) == "table" then
-			if reqMod.tick then table.insert(doTick, reqMod) end
-			if reqMod.render then table.insert(doRender, reqMod) end
+			if reqMod.tick then table.insert(doTick, reqMod) end		-- find all libraries with a tick event
+			if reqMod.render then table.insert(doRender, reqMod) end	-- find all libraries with a render event
 		end
 	end
 end
 
-function events.tick()
+function events.tick() -- does tick event for all libraries that have it
 	for _, m in ipairs(doTick) do
 		for _, inst in ipairs(m.instances) do
 	        inst:tick()
@@ -52,7 +52,7 @@ function events.tick()
 	end
 end
 
-function events.render(delta, context)
+function events.render(delta, context) -- does render event for all libraries that have it
 	for _, m in ipairs(doRender) do
 		for _, inst in ipairs(m.instances) do
 	        inst:render(delta, context)
@@ -61,3 +61,4 @@ function events.render(delta, context)
 end
 
 return bp
+-- a full stack of lines B^)
